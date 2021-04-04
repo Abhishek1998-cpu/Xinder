0938f002a09ff64cc6aaffe4c13e335bc88889c2
Abhishek <abhishekverma998@gmail.com>
2021-04-04 15:47:11 +0530

    First

---
+++ tinder_frontend/src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./App.css";
+import Header from "./Header.js";
+import TinderCards from "./TinderCards";
+import SwipeButtons from "./SwipeButtons";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <h1>Tinder Clone MERN Stack</h1> */}
+      {/* Header */}
+      <Header />
+      {/* TinderCards */}
+      <TinderCards />
+      {/* SwipeButtons */}
+      <SwipeButtons />
+    </div>
+  );
+}
+
+export default App;
+
+// 58.22
+/* 1.07  */
+// 2:11
